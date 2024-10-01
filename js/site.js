27b28d7d6f05c9986ffd1d2baff5308732ba8fa5
Al Fatah Rizki Ananda <144166775+alfatahrzk@users.noreply.github.com>
2024-10-01 02:41:52 +0000

    Update index.html

--- js/site.js
+++ js/site.js
@@ -63,5 +63,74 @@ $(window).load(function () { // makes sure the whole site is loaded
 	if (!!$.prototype.enllax) {
 		$(window).enllax();
 	}
+/* Cek Rekening */
+function cekRekening() {
+    // Ambil nilai dari input rekening
+    const rekening = document.getElementById('input1').value;
 
+    // logika
+    if (rekening === '1234567890') {
+        isPenipuan = true;
+    } else {
+        isPenipuan = false;
+    }
+
+    if (isPenipuan) {
+    hasilCek.textContent = "Nomor rekening terindikasi sebagai penipuan!";
+  } else {
+    hasilCek.textContent = "Nomor rekening aman.";
+  }
+}
+
+/* Cek Password */
+function cekPassword() {
+    // Ambil nilai dari input password
+    const password = document.getElementById('input2').value;
+
+	// logika
+    if (password.length < 8) {
+        alert('Password terlalu pendek. Minimal 8 karakter.');
+    } else if (!/[A-Z]/.test(password)) {
+        alert('Password harus mengandung minimal 1 huruf kapital.');
+    } else if (!/[a-z]/.test(password)) {
+        alert('Password harus mengandung minimal 1 huruf kecil.');
+    } else if (!/\d/.test(password)) {
+        alert('Password harus mengandung minimal 1 angka.');
+    } else {
+        alert('Password cukup kuat.');
+    }
+}
+    
+/* Data Grafik */
+    const labels = ['Penipuan Online', 'Ancaman Kekerasan', 'Pencemaran Nama Baik', 'Ancaman Pencemaran'];
+    const data = {
+        labels: labels,
+        datasets: [{
+            label: 'Jumlah Kejahatan yang telah Dilaporkan',
+            data: [14495, 8614, 6556, 3675],
+            backgroundColor: 'rgb(126, 96, 191)',
+            borderColor: 'rgba(255, 99, 132, 1)',
+            borderWidth: 1
+        }]
+    };
+
+    // Konfigurasi chart
+    const config = {
+        type: 'bar',
+        data: data,
+        options: {
+            scales: {
+                y: {
+                    beginAtZero: true
+                }
+            },
+            indexAxis: 'y'
+        }
+    };
+
+    // Membuat chart
+    const myChart = new Chart(
+        document.getElementById('myChart'),
+        config
+    );
 });
